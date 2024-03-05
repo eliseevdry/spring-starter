fb416f3d2a1c145a6c972c48c6e20a6d0f330c17
Andrey Eliseev <andrey.s.eliseev@lenta.com>
2024-03-05 08:04:57 +0300

    lesson-91: Validation Starter. Введение

--- src/main/java/org/example/dto/UserCreateEditDto.java
+++ src/main/java/org/example/dto/UserCreateEditDto.java
@@ -3,18 +3,27 @@ package org.example.dto;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
 import org.example.database.entity.Role;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Value
 @FieldNameConstants
 public class UserCreateEditDto {
+    @Email
     String username;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     LocalDate birthDate;
 
+    @NotNull
+    @Size(min = 3, max = 64)
     String firstname;
 
+    @NotNull
     String lastname;
 
     Role role;

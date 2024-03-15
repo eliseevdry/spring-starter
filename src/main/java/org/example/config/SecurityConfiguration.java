9f6d9d6c5ed436d2f8630e3e8308455ac4ca4c01
Andrey Eliseev <andrey.s.eliseev@lenta.com>
2024-03-15 17:03:19 +0300

    lesson-113: OAuth 2.0. Authentication Principle

--- src/main/java/org/example/config/SecurityConfiguration.java
+++ src/main/java/org/example/config/SecurityConfiguration.java
@@ -1,18 +1,29 @@
 package org.example.config;
 
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
+import org.example.service.UserService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Set;
 
 import static org.example.database.entity.Role.ADMIN;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -34,11 +45,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .defaultSuccessUrl("/users"))
                 .oauth2Login(login -> login
                         .loginPage("/login")
-                        .defaultSuccessUrl("/users"));
+                        .defaultSuccessUrl("/users")
+                        .userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService())));
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return userRequest -> {
+            String email = userRequest.getIdToken().getEmail();
+            // TODO Create user userService.create
+            UserDetails userDetails = userService.loadUserByUsername(email);
+//            return new OidcUserService().loadUser(userRequest);
+            DefaultOidcUser oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+            Set<Method> userDetailsMethods = Set.of(UserDetails.class.getMethods());
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{UserDetails.class, OidcUser.class},
+                    ((proxy, method, args) -> userDetailsMethods.contains(method)
+                            ? method.invoke(userDetails, args)
+                            : method.invoke(oidcUser, args)));
+        };
     }
 }
